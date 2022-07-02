ede323b31814107f29c9443810c2c0a8c1d9a8ca
anil villamkandathil <vnanil@gmail.com>
2022-07-02 16:28:55 -0400

    Javascript practice

---
+++ HTML/javascript.js
@@ -0,0 +1,42 @@
+console.log("hello");
+var number = 5;    //in-line comment 
+var myName = "Beau";
+myName = 8;
+let ourName = "freecodeCamp";
+var a;
+var b =2;
+a = 7;
+b = a;
+console.log(a);
+var a = 9;
+var b = 10;
+var c = "I am a";
+var sum = 10 + 0;
+console.log(sum);
+var difference = 45 - 33;
+var product = 8 * 10;
+var quotient = 66 / 33;
+var myVar = 87; 
+myVar = myVar + 1;
+myVar++;
+myVar--;
+var decimal = 5.7;
+var product = 2.0 * 2.5;
+console.log(product);
+var remainder;
+remainder = 11 % 3; 
+var myStr = "I am a double quoted";
+var myArray = [["Bulls",23]];
+myArray.push("Bla",39);
+var myList = [["cereal",3],["milk",2],["bananas",3]];
+function ourReusableFunction() {
+    console.log("Heyya, World";)
+}
+ourReusableFunction();
+function reusableFunction() {
+    console.log("hi World");
+}
+function ourFunctionWithArgs(a,b){
+    console.log(a-b);
+}
+ourFunctionWithArgs(10,5);
