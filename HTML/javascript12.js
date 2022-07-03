dadb57fdef4a6de86f4b22b915244200fb740490
anil villamkandathil <vnanil@gmail.com>
2022-07-03 11:00:12 -0400

    JS objects

---
+++ HTML/javascript12.js
@@ -0,0 +1,13 @@
+var ourDog = {
+    "name" : "Camper",
+    "tail" : 1,
+    "legs" : 4, 
+     "friends" : ["everything!"]
+};
+
+var myDog = {
+    "name" : "Quincy",
+    "tail" : 1,
+    "legs" : 3,
+    "friends" : []
+};
