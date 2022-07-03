0e7dd18394637f5c7b1d72503ac51bb8e821e355
anil villamkandathil <vnanil@gmail.com>
2022-07-03 11:12:11 -0400

    access object fields with square brackes

---
+++ HTML/javascript14.js
@@ -0,0 +1,8 @@
+var testObj = {
+    "an entree" : "hamburger",
+    "my side"  : "veggies",
+    "the drink" : "water"
+ };
+
+ var entreevalue  = testObj["an entree"];
+ var drinkValue = testObj["the drink"];
